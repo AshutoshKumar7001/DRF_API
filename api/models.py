9b00416a3733989ba63cd6c1a5175ce741465ff0
ASHUTOSH KUMAR <ASHUTOSHKUMAR7001@GMAIL.COM>
2023-07-11 14:42:47 +0530

    updated api

---
+++ api/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+# Create your models here.
+# Creating Company Model
+
+class Company(models.Model):
+    Company_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    about = models.TextField()
+    type = models.CharField(max_length=100, choices=
+                            (('IT', 'IT'),
+                              ('Non IT', 'Non IT'),
+                              ('Mobile','Mobile')
+                              ))
+    added_data = models.DateTimeField(auto_now = True)
+    active = models.BooleanField(default=True)
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=50)
+    address = models.CharField(max_length=200)
+    phone = models.CharField(max_length=10)
+    about = models.TextField()
+    position = models.CharField(max_length=50, choices=(
+        ('Manager', 'manager'),
+        ('Software Developer', 'sd'),
+        ('Project Leader', 'pl')
+    ))
+
+company = models.ForeignKey(Company, on_delete=models.CASCADE)
