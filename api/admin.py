b7f5b5c5dbb83946424203fff07b1ef53e3a25cb
ASHUTOSH KUMAR <ASHUTOSHKUMAR7001@GMAIL.COM>
2023-07-11 18:57:36 +0530

    search field added into the admin pannel for the company as well as employee

--- api/admin.py
+++ api/admin.py
@@ -4,9 +4,11 @@ from api.models import Company, Employee
 
 class CompanyAdmin(admin.ModelAdmin):
     list_display=('name', 'location', 'type')
+    search_fields=('name',)
 
 class EmployeeAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'phone', 'position')
+    search_fields=('name', 'email', 'phone',)
 
 admin.site.register(Company, CompanyAdmin)
 admin.site.register(Employee, EmployeeAdmin)
